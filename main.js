b851ec89971164a988360359a813fbc1ecb84672
sammickns <bluhead.dev@gmail.com>
2025-05-20 11:28:26 -0500

    simplifique argumentos

--- main.js
+++ main.js
@@ -5,31 +5,13 @@ let gramatica = {
     intro: ["La ley integral trans es importante para la <seccion>"],
     conector: ["de las personas trans porque"],
     seccion: ["salud", "educación", "deporte", "identidad", "vivienda"],
-    argumento: ["$seccionSalud$[obj:argumentosSalud]<seccionSalud.obj>"],
-    argumentosSalud: [
-      "",
-      "",
-      "",
-      ""
-    ],
-    argumentosEducacion: [
-      "",
-      "",
-      "",
-      ""
-    ],
-    argumentosIdentidad: [
-      "",
-      "",
-      "",
-      ""
-    ],
-    argumentosDeporte: [
-      "",
-      "",
-      "",
-      ""
-    ],
+    argumento: [
+      "reconoce legalmente su identidad, fomentando el respeto y la inclusión social en todos los ámbitos vitales.",
+      "elimina barreras sistémicas, facilitando el acceso a oportunidades y servicios esenciales para una vida digna.",
+      "ofrece un marco legal que combate la discriminación, promoviendo la igualdad real y efectiva de derechos.",
+      "contribuye a su bienestar integral, al garantizar un entorno más seguro y protector de sus derechos humanos.",
+      "valida su existencia y sus derechos fundamentales, permitiendo un desarrollo personal y social pleno y libre."
+    ]
   }
 
   let texto = aventura.fijarGramatica(gramatica).probarGramatica(gramatica).expandirGramatica('mensaje');
